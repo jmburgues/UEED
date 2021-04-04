0984671d621f1e458fb794b6fde1e675e5daa72e
Juan Manuel Burgues Schaab <securetux@gmail.com>
2021-04-03 21:28:10 -0300

    Spring project initialization

---
+++ src/main/java/edu/utn/ueed/UeedApplication.java
@@ -0,0 +1,13 @@
+package edu.utn.ueed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class UeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UeedApplication.class, args);
+	}
+}
---
+++ src/test/java/edu/utn/ueed/UeedApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.utn.ueed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UeedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
